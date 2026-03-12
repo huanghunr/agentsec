463d600f467f47507abf609263c8c5a5353eb1f9
huanghunr <huanghunr@outlook.com>
2026-03-12 05:16:29 +0000

    new git info

--- home/home.nix
+++ home/home.nix
@@ -1,6 +1,20 @@
-{pkgs,...}:{
+{ pkgs, ... }:
+{
   home.username = "nixos";
   home.homeDirectory = "/home/nixos";
+  
+  programs = {
+    git = {
+      enable = true;
+      settings = {
+        user.name = "huanghunr";
+        user.email = "huanghunr@outlook.com";
+        gpg = {
+          format = "ssh";
+        };
+      };
+    };
+  };
 
   home.stateVersion = "25.11";
 }
